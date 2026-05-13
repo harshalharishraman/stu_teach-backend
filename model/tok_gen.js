4fd786f5539c9a3fa1031d158d6de1342c5a8fcb
harshalharishraman <harshal.ixa@gmail.com>
2026-05-13 23:23:43 +0530

    feat:added access token auth in login

--- model/tok_gen.js
+++ model/tok_gen.js
@@ -1,4 +1,5 @@
 const j=require('jsonwebtoken')
+const re = require('../resvo/res_t');
 class tok{
     static async acc_t_gen(n,e,p){
         try{
@@ -16,6 +17,21 @@ class tok{
             console.error('error in generationg access token:',error)
             throw error
         }
+    }
+    static async jwt_verify(req,res,next){
+try {const req_head_auth=req.headers.authorization
+    if(!req_head_auth){
+        return res.status(400).json(null,400,'header missing')
+    }
+    const acc_tk=req_head_auth.split()[1]
+    if(!acc_tk){
+        return res.status(400).json(null,400,'token missing')}
+    const dec=j.verify(acc_tk,process.env.access_sec_k)
+    req.user=dec
+    next();  
+} catch (error) {
+    res.status(500).json(new re(null,500,'invalid token'))
+}
     }
 
 }
--- view/router_t.js
+++ view/router_t.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const exp = require('express');
 const c=require('../controls/controller_t');
 const rter=exp.Router();
-
+const t=require('../model/tok_gen')
+rter.use(t.jwt_verify)
 rter.post('/signup_t',c.c_tacc)
 rter.get('/login_t',c.l_tacc)
 module.exports=rter
